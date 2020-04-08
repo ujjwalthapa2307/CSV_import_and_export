ae3474497e82b7c78cc5879b1780e8e11d037bb9
Thapa <ujjwalthapa2307@gmail.com>
2020-04-08 12:56:32 +0900

    Export data from database into CSV format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html
+      format.csv { send_data @users.to_csv }
+    end
   end
   def import
     User.import(params[:file])
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,14 @@ class User < ApplicationRecord
   def self.updatable_attributes
     ["id", "name", "age"]
   end
+
+  def self.to_csv
+    attributes = %w{id name age}
+    CSV.generate(headers: true) do |csv|
+      csv << attributes
+      all.each do |user|
+        csv << user.attributes.values_at(*attributes)
+      end
+    end
+  end
 end
